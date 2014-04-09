53a2a1a1cedcb82b800f3f3bf4052ad2fc60d8e7
lauradegroot <laurardegroot@gmail.com>
2014-04-08 21:37:51 -0400

    more accurate countdown to meatconfe

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(function() {
 
 var countdownToMeatups = function(){
 	
-	var meatupEast = new Date(2014, 03, 10);
+	var meatupEast = new Date("2014-04-10T22:00:00");
 	$('.east .count').countdown({until: meatupEast});
 	
 	var meatupWest = new Date(2014, 04, 9);
