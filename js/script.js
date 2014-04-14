0b66ceaf84939bbaa590e724555970752693014c
lauradegroot <laurardegroot@gmail.com>
2014-04-14 09:36:25 -0400

    update because meatconfE is over

--- js/script.js
+++ js/script.js
@@ -6,12 +6,6 @@ $(function() {
 
 var countdownToMeatups = function(){
 	
-	var meatupEast = new Date("2014-04-10T22:00:00");
-	$('.east .count').countdown({
-		until: meatupEast,
-		alwaysExpire: true,
-		expiryText:'<img src="http://i.imgur.com/i2yIDSk.gif"/>'});
-	
 	var meatupWest = new Date(2014, 04, 9);
 	$('.west .count').countdown({until: meatupWest});
 
