310c1c90baca0f00f929ae2eca5b8a58f68b2dc9
lauradegroot <laurardegroot@gmail.com>
2014-08-18 22:12:04 -0400

    update date for sf meatup

--- js/script.js
+++ js/script.js
@@ -9,6 +9,6 @@ var countdownToMeatups = function(){
 	var meatupWest = new Date(2014, 9, 2);
 	$('.east .count').countdown({until: meatupWest});
 
-	var meatupEu = new Date(2014, 9, 19);
+	var meatupEu = new Date(2014, 9, 18);
 	$('.west .count').countdown({until: meatupEu});
 }
